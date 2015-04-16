2aa1678e76be99d54688d590761ac93012b7c16b
vmalarcon <vmalarcon@gmail.com>
2015-04-15 20:24:21 -0400

    Some progress with assignment2

--- assignment2/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ assignment2/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -19,9 +19,26 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     findMin(insert(m, h)) == m
   }
 
+  property("meld3") = forAll { (h: H, i: H) =>
+    val hmin = findMin(h)
+    val imin = findMin(i)
+
+    if (hmin < imin) findMin(meld(h, i)) == hmin
+    else findMin(meld(h, i)) == imin
+  }
+
+  property("del2") = forAll { (h: H) =>
+    validate(Int.MinValue, h)
+  }
+
+  def validate(p: Int, heap: H): Boolean = {
+    if (!isEmpty(heap)) findMin(heap) >= p && validate(findMin(heap), deleteMin(heap))
+    else true
+  }
+
   lazy val genHeap: Gen[H] = for {
     a <- arbitrary[Int]
-    h <- if (a % 1000 == 0) const(empty) else genHeap
+    h <- if (a % 10 == 0) const(empty) else genHeap
   } yield insert(a, h)
 
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
