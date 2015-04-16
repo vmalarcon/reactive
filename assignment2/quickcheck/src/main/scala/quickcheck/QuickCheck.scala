17160409a962bca689c793e58b046e9de07f7b7b
valarcon <valarcon@expedia.com>
2015-04-16 10:17:46 -0400

    Submitted assignment2

--- assignment2/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ assignment2/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -19,7 +19,7 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     findMin(insert(m, h)) == m
   }
 
-  property("meld3") = forAll { (h: H, i: H) =>
+  property("meld1") = forAll { (h: H, i: H) =>
     val hmin = findMin(h)
     val imin = findMin(i)
 
@@ -27,13 +27,90 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     else findMin(meld(h, i)) == imin
   }
 
-  property("del2") = forAll { (h: H) =>
-    validate(Int.MinValue, h)
+  property("val5") = forAll { (h: H) =>
+    validate(findMin(h), deleteMin(h))
+  }
+
+  property("min2") = forAll { (a: Int, b: Int) =>
+    val ah = insert(a, empty)
+    val bh = insert(b, ah)
+
+    if (a < b) findMin(bh) == a
+    else findMin(bh) >= b
+  }
+
+  property("empty1") = forAll { a: Int =>
+    val h = insert(a, empty)
+    val dh = deleteMin(h)
+
+    isEmpty(dh)
+  }
+
+  property("meld2") = forAll { (h: H, i: H) =>
+    val hi = meld(h, i)
+    val ih = meld(i, h)
+
+    heapEquals(hi, ih)
+  }
+
+  property("meld3") = forAll { (h: H, i: H) =>
+    val hi = meld(h, i)
+    val ih = meld(i, h)
+
+    (heapSize(h) + heapSize(i)) == heapSize(hi) && (heapSize(h) + heapSize(i)) == heapSize(ih)
+  }
+
+  property("meld4") = forAll { (h: H, i: H) =>
+    val hi = meld(h, i)
+    val ih = meld(i, h)
+
+    val hiih = meld(hi, ih)
+    val ihhi = meld(ih, hi)
+
+    validate(findMin(hiih), deleteMin(hiih)) && validate(findMin(ihhi), deleteMin(ihhi))
+  }
+
+  property("meld5") = forAll { (h: H, i: H) =>
+    val hi = meld(h, i)
+    val ih = meld(i, h)
+
+    val hiih = meld(hi, ih)
+    val ihhi = meld(ih, hi)
+
+    heapEquals(hiih, ihhi)
+  }
+
+  property("order1") = forAll { l: List[Int] =>
+    val h = fillHeap(l)
+    val ls = l.sortWith((a: Int, b: Int) => a < b)
+
+    compareWithList(h, ls)
   }
 
   def validate(p: Int, heap: H): Boolean = {
-    if (!isEmpty(heap)) findMin(heap) >= p && validate(findMin(heap), deleteMin(heap))
-    else true
+    if (isEmpty(heap)) return true
+    else findMin(heap) >= p && validate(findMin(heap), deleteMin(heap))
+  }
+
+  def heapEquals(h: H, i: H): Boolean = {
+    if (isEmpty(h) && isEmpty(i)) return true
+    else if (isEmpty(h) || isEmpty(i)) return false
+    else findMin(h) == findMin(i) && heapEquals(deleteMin(h), deleteMin(i))
+  }
+
+  def heapSize(h: H): Int = {
+    if (isEmpty(h)) 0 else 1 + heapSize(deleteMin(h))
+  }
+
+  def fillHeap(l: List[Int]): H = l match {
+    case Nil => empty
+    case x::xs => insert(x, fillHeap(xs))
+  }
+
+  def compareWithList(h: H, l: List[Int]): Boolean = {
+    if (isEmpty(h) && l.isEmpty) true
+    else if (isEmpty(h) || l.isEmpty) false
+    else findMin(h) == l.head && compareWithList(deleteMin(h), l.tail)
   }
 
   lazy val genHeap: Gen[H] = for {
