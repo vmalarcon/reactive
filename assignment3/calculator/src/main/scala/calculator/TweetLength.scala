185ae0fa513c861768179151fc610f506fbaa65b
valarcon <valarcon@expedia.com>
2015-04-20 17:43:37 -0400

    Implemented first function

--- assignment3/calculator/src/main/scala/calculator/TweetLength.scala
+++ assignment3/calculator/src/main/scala/calculator/TweetLength.scala
@@ -4,7 +4,7 @@ object TweetLength {
   final val MaxTweetLength = 140
 
   def tweetRemainingCharsCount(tweetText: Signal[String]): Signal[Int] = {
-    ???
+    Var(MaxTweetLength - tweetLength(tweetText()))
   }
 
   def colorForRemainingCharsCount(remainingCharsCount: Signal[Int]): Signal[String] = {
