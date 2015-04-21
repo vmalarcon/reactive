bfd41edc7d8c3cea677a30cdc81394bcdd20266e
valarcon <valarcon@expedia.com>
2015-04-21 11:53:38 -0400

    Implement Polynomial

--- assignment3/calculator/src/main/scala/calculator/Polynomial.scala
+++ assignment3/calculator/src/main/scala/calculator/Polynomial.scala
@@ -3,11 +3,18 @@ package calculator
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
       c: Signal[Double]): Signal[Double] = {
-    ???
+    Var(Math.pow(b(), 2.0) - 4 * a() * c())
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+    if (delta() < 0 || a() == 0) Var(Set())
+    else {
+      val root1 = Var((-b() - Math.sqrt(delta())) / (2 * a()))
+      val root2 = Var((-b() + Math.sqrt(delta())) / (2 * a()))
+
+      if (root1 == root2) Var(Set(root1()))
+      else Var(Set(root1(), root2()))
+    }
   }
 }
---
+++ assignment3/calculator/src/test/scala/calculator/PolynomialSuite.scala
@@ -0,0 +1,95 @@
+package calculator
+
+import org.junit.runner.RunWith
+import org.scalatest.{FunSuite, _}
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class PolynomialSuite extends FunSuite with ShouldMatchers {
+
+  test("test basic discriminant") {
+    val a = Var(1.0)
+    val b = Var(1.0)
+    val c = Var(1.0)
+
+    val delta = Polynomial.computeDelta(a, b, c)
+
+    assert(delta() === -3.0)
+
+    b() = -1.0
+
+    assert(delta() === -3.0)
+
+    b() = 2.0
+
+    assert(delta() === 0)
+
+    c() = -1.0
+
+    assert(delta() === 8)
+
+    a() = -1.0
+
+    assert(delta() === 0)
+  }
+
+  test("test no solutions") {
+    val a = Var(1.0)
+    val b = Var(1.0)
+    val c = Var(1.0)
+
+    val delta = Polynomial.computeDelta(a, b, c)
+
+    assert(delta() < 0)
+
+    val sol = Polynomial.computeSolutions(a, b, c, delta)
+
+    assert(0 === sol().size)
+
+    a() = 0.0
+
+    assert(0 === sol().size)
+  }
+
+  test("test one solution") {
+    val a = Var(1.0)
+    val b = Var(2.0)
+    val c = Var(1.0)
+
+    val delta = Polynomial.computeDelta(a, b, c)
+
+    assert(delta() === 0)
+
+    val sol = Polynomial.computeSolutions(a, b, c, delta)
+
+    assert(1 === sol().size)
+    assert(-1.0 === sol().head)
+  }
+
+  test("test two solutions") {
+    val a = Var(1.0)
+    val b = Var(-2.0)
+    val c = Var(0.0)
+
+    val delta = Polynomial.computeDelta(a, b, c)
+
+    assert(delta() === 4.0)
+
+    val sol = Polynomial.computeSolutions(a, b, c, delta)
+
+    assert(2 === sol().size)
+    assert(0.0 === sol().head)
+    assert(2.0 === sol().tail.head)
+
+    a() = -2.0
+    b() = -3.0
+    c() = 2.0
+
+    assert(delta() === 25.0)
+
+    assert(2 === sol().size)
+    assert(0.5 === sol().head)
+    assert(-2.0 === sol().tail.head)
+  }
+
+}
