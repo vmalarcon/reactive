9b76369c2cb4323e215ac818e40abc0c09a83438
vmalarcon <vmalarcon@gmail.com>
2015-04-29 07:20:12 -0400

    Starting to work on assignment

--- assignment5/nodescala/nodescala/src/main/scala/nodescala/package.scala
+++ assignment5/nodescala/nodescala/src/main/scala/nodescala/package.scala
@@ -17,12 +17,12 @@ package object nodescala {
 
     /** Returns a future that is always completed with `value`.
      */
-    def always[T](value: T): Future[T] = ???
+    def always[T](value: T): Future[T] = Future {value}
     /** Returns a future that is never completed.
      *
      *  This future may be useful when testing if timeout logic works correctly.
      */
-    def never[T]: Future[T] = ???
+    def never[T]: Future[T] = Future.failed(new TimeoutException)
     /** Given a list of futures `fs`, returns the future holding the list of values of all the futures from `fs`.
      *  The returned future is completed only once all of the futures in `fs` have been completed.
      *  The values in the list are in the same order as corresponding futures `fs`.
